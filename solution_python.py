3f4f726e99fa4d10b0217c37ef12a6fc2b2adcb0
H-H2648 <46250599+H-H2648@users.noreply.github.com>
2020-07-10 11:32:03 -0700

    Revert "Update solution_python.py"
    This reverts commit 34c376a267271246ed5220fc83db5d608047f25f.

--- solution_python.py
+++ solution_python.py
@@ -3,61 +3,21 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
-        #historyToUndo and historyToRedo keep tracks of all the actions
-        self.historyToUndo = []
-        self.historyToRedo = []
 
     def add(self, num: int):
-        #Becuase we have done something, we keep track of the action to historyToUndo
-        self.historyToUndo.append(num)
-        self.value = self.value + num
+        pass
 
-    #subtracting is equivalent to adding negative
     def subtract(self, num: int):
-        num = -num
-        self.add(num)
-    
+        pass
 
     def undo(self):
-        try:
-            #we negate the most recent action (by subtracting it [since we added]) in historyToUndo
-            self.value = self.value - self.historyToUndo[-1]
-            #We keep track of the undo in historyToRedo
-            self.historyToRedo.append(self.historyToUndo[-1])
-            #We no longer need to keep track of that action in historyToUndo
-            self.historyToUndo.pop()
-        #If we can't undo, we do nothing
-        except:
-            pass
-
+        pass
 
     def redo(self):
-        try:
-            #We negate the most recent redo (by adding it [since we subtracted]) in historyToRedo
-            self.value = self.value + self.historyToRedo[-1]
-            #We keep track of the redo in historyToUndo
-            self.historyToUndo.append(self.historyToRedo[-1])
-            #We no longer need to keep track of that action in historyToRedo
-            self.historyToRedo.pop()
-        #If we can't redo, we do nothing
-        except:
-            pass
-
+        pass
 
     def bulk_undo(self, steps: int):
-        index = steps
-        #We undo until we can't do it anymore
-        while (index > 0 and len(self.historyToUndo) > 0):
-            self.undo()
-            print(self.value)
-            index -= 1
-
+        pass
 
     def bulk_redo(self, steps: int):
-        index = steps
-        #We redo until we can't do it anymore
-        while (index > 0 and len(self.historyToRedo) > 0):
-            self.redo()
-            index -=1
-
-
+        pass
